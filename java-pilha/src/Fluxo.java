d01025a43bcbb3afff2c4cdc5cc36a44e43a644f
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-01 12:14:23 -0300

    aula 2 completa

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,14 +1,20 @@
-
+//Aula 2 completa
 public class Fluxo {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception " + msg);
+			ex.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-			metodo2();
+		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
@@ -17,6 +23,8 @@ public class Fluxo {
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
 			int a = i / 0;
+//			Conta c = null;
+//			c.deposita();
 		}
 		System.out.println("Fim do metodo2");
 	}
