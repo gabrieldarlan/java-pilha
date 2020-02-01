7f9bedf0a12781ac8d38e729910689aa3593daa6
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-01 12:32:44 -0300

    Capitulo 3 Lancando exceçoes

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,5 @@
 //Aula 2 completa2
-public class Fluxo {
+public class FluxoComTratamento {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try {
@@ -20,12 +20,7 @@ public class Fluxo {
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-			int a = i / 0;
-//			Conta c = null;
-//			c.deposita();
-		}
-		System.out.println("Fim do metodo2");
+		throw new ArithmeticException("deu erro");
+//		System.out.println("Fim do metodo2");
 	}
 }
